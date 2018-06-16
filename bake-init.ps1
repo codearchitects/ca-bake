8b0ffce33d8535f0b40574201a2f534e3708ccc9
Nicola Gallo <nicolagmt@hotmail.com>
2018-06-17 00:04:21 +0100

    Renamed files

--- bake-init.ps1
+++ bake-init.ps1
@@ -1,8 +1,8 @@
 $urlFile = [Environment]::GetEnvironmentVariable("BAKE_RUNNER_URL")
 if([string]::IsNullOrEmpty($urlFile)) 
 {
-    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-setup.ps1"
+    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-install.ps1"
 }
-Invoke-WebRequest $urlFile -OutFile bake-setup.ps1
-.\bake-setup.ps1
+Invoke-WebRequest $urlFile -OutFile bake-install.ps1
+.\bake-install.ps1
 .\bake-run.ps1 -step CI
