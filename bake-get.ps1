25ec4bc0e0ea90c3d482435eaa904ddd3abfbff1
Nicola Gallo <nicolagmt@hotmail.com>
2018-06-17 00:00:40 +0100

    Fixed bake-get

--- bake-get.ps1
+++ bake-get.ps1
@@ -3,6 +3,6 @@ if([string]::IsNullOrEmpty($urlFile))
 {
     $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-setup.ps1"
 }
-Invoke-WebRequest $urlFile -OutFile build-setup.ps1
-.\build-setup.ps1
-.\build-run.ps1 -step CI
+Invoke-WebRequest $urlFile -OutFile bake-setup.ps1
+.\bake-setup.ps1
+.\bake-run.ps1 -step CI
