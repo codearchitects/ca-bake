9c22d6a06aea5a111832bdef1473a3495b928045
Nicola Gallo <nicolagmt@hotmail.com>
2018-06-21 13:27:47 +0100

    Implemented logic to install only the modules which are not installed

--- bake-install.ps1
+++ bake-install.ps1
@@ -1,3 +1,9 @@
 Invoke-WebRequest https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-run.ps1 -OutFile "bake-run.ps1"
-Install-PackageProvider -Scope CurrentUser  -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -force -Scope CurrentUser psyaml -AllowClobber
+if (Get-Module -ListAvailable -Name psyaml) {
+    Write-Host "Module psyaml exists"
+} else {
+    Write-Host "Installing psyaml module"
+    Install-PackageProvider -Scope CurrentUser  -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Module -force -Scope CurrentUser psyaml -AllowClobber
+    Write-Host "Installed psyaml module"
+}
