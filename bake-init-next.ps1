63fb5f2213422cbddc05e4fea776e3601f849725
Federico Silletti <fsilletti@codearchitects.com>
2018-09-24 10:51:36 +0200

    fix bake-install-next

--- bake-init-next.ps1
+++ bake-init-next.ps1
@@ -1,7 +1,7 @@
 $urlFile = [Environment]::GetEnvironmentVariable("BAKE_RUNNER_URL")
 if ([string]::IsNullOrEmpty($urlFile)) {
-    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-install-next.ps1"
+    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-install_next.ps1"
 }
-Invoke-WebRequest $urlFile -OutFile bake-install-next.ps1
-.\bake-install-next.ps1
-Remove-Item bake-install-next.ps1
+Invoke-WebRequest $urlFile -OutFile bake-install_next.ps1
+.\bake-install_next.ps1
+Remove-Item bake-install_next.ps1
