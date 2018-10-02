e5b3a3add3669de8c2e4acb0da77150c87c8aec4
Carmine <cdente@codearchitects.com>
2018-10-02 15:21:31 +0200

    .

--- bake-init.ps1
+++ bake-init.ps1
@@ -1,6 +1,6 @@
 $urlFile = [Environment]::GetEnvironmentVariable("BAKE_RUNNER_URL")
 if ([string]::IsNullOrEmpty($urlFile)) {
-    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-install.ps1"
+    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/bake-init.ps1?$($(Get-Date).ToFileTimeUTC())"
 }
 Invoke-WebRequest $urlFile -OutFile bake-install.ps1
 .\bake-install.ps1
