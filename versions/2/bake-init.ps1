832fd09c79ec06b1bbbc0375aa45fb01d782e98a
fsilletti <41385085+fsilletti@users.noreply.github.com>
2019-03-12 14:56:20 +0100

    Fix Bake-init (#25)

--- versions/2/bake-init.ps1
+++ versions/2/bake-init.ps1
@@ -1,15 +1,9 @@
 $ErrorActionPreference = "Stop"
 $global:ProgressPreference = "SilentlyContinue"
 $urlFile = [Environment]::GetEnvironmentVariable("BAKE_RUNNER_URL")
-import-module psyaml
-[string[]]$fileContent = Get-Content ".\bake-recipe.yml"
-$content = ''
-foreach ($line in $fileContent) { $content = $content + "`n" + $line }
-$yaml = ConvertFrom-YAML $content
-$version = $yaml["version"]
-if ([string]::IsNullOrEmpty($urlFile)) {
-    $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/versions/$version/bake-install.ps1?$($(Get-Date).ToFileTimeUTC())"
-}
+if (-Not (Get-Module -ListAvailable -Name psyaml)) { Install-PackageProvider -Scope CurrentUser -Name NuGet -MinimumVersion 2.8.5.201 -Force; Install-Module -force -Scope CurrentUser psyaml -AllowClobber; }; import-module psyaml
+[string[]]$fileContent = Get-Content ".\bake-recipe.yml"; foreach ($line in $fileContent) { $content = $content + "`n" + $line }; $yaml = ConvertFrom-YAML $content; $version = $yaml["version"]
+if ([string]::IsNullOrEmpty($urlFile)) { $urlFile = "https://raw.githubusercontent.com/codearchitects/ca-bake/master/versions/$version/bake-install.ps1?$($(Get-Date).ToFileTimeUTC())" }
 Invoke-WebRequest $urlFile -OutFile bake-install.ps1
 .\bake-install.ps1
 Remove-Item bake-install.ps1
